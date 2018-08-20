e0fe1f36c745363aa75362847ff527acf1ed564e
Carlos Elguedo <carlos-elguedo@hotmail.com>
2018-08-20 16:36:12 -0500

    First commit

---
+++ src/database.js
@@ -0,0 +1,19 @@
+/**
+ * @author Carlos Elguedo
+ * @version 0.0.1
+ * File responsible for establishing the connection to the database
+ */
+
+const mongoose = require('mongoose')
+//Global configurations
+const config = require('./config')
+
+//We define the url to connect to the bd
+const URL = config.DATABASE_URL
+
+//We connect to the database provided
+mongoose.connect(URL, { useNewUrlParser: true })
+    .then(db => console.log('Connect to mongodb'))
+    .catch(err => console.error("Error --------------->" + err))
+
+module.exports = mongoose
---
+++ src/index.js
@@ -0,0 +1,42 @@
+/**
+ * @author Carlos Elguedo
+ * @version 0.0.1
+ * Main file of the project
+ */
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+const {mongoose} = require('./database')
+
+
+
+
+
+//Setting
+//We tell the app to congigure to use the port provided by the operating system
+app.set('port', process.env.PORT || 3001)
+
+
+
+
+//Middlewares
+//We indicate that we will work with data in json format
+app.use(express.json());
+//To see details of the requests
+app.use(morgan('dev'))
+
+
+
+
+//Routes
+//Route of the api providers
+app.use('/api/rest/login', require('./routes/accessrouter'))
+
+
+
+
+//Starting the server
+app.listen(app.get('port'), ()=>{
+    console.log(`server started at port ${app.get('port')}`);
+});
